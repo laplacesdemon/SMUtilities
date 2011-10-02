ee2070d3779c685a364138a9bb84a595558de269
Suleyman Melikoglu <suleyman@melikoglu.info>
2011-10-02 19:11:39 +0300

    Initial Commit

---
+++ SMUtilitiesTests/SMUtilitiesTests.h
@@ -0,0 +1,13 @@
+//
+//  SMUtilitiesTests.h
+//  SMUtilitiesTests
+//
+//  Created by Suleyman Melikoglu on 10/2/11.
+//  Copyright 2011 suleymanmelikoglu@gmail.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SMUtilitiesTests : SenTestCase
+
+@end
